9586da1e5e324fe405e56cad90a78d50819c43df
kpcyrd <git@rxv.cc>
2018-03-27 18:44:24 +0200

    Make print() easier to read

--- src/runtime.rs
+++ src/runtime.rs
@@ -191,11 +191,46 @@ pub fn mysql_connect(lua: &mut hlua::Lua, _state: State) {
     }))
 }
 
+fn format_lua(out: &mut String, x: &AnyLuaValue) {
+    match *x {
+        AnyLuaValue::LuaNil => out.push_str("null"),
+        AnyLuaValue::LuaString(ref x) => out.push_str(&format!("{:?}", x)),
+        AnyLuaValue::LuaNumber(ref x) => out.push_str(&format!("{:?}", x)),
+        AnyLuaValue::LuaAnyString(ref x) => out.push_str(&format!("{:?}", x.0)),
+        AnyLuaValue::LuaBoolean(ref x) => out.push_str(&format!("{:?}", x)),
+        AnyLuaValue::LuaArray(ref x) => {
+            out.push_str("{");
+            let mut first = true;
+
+            for &(ref k, ref v) in x {
+                if !first {
+                    out.push_str(", ");
+                }
+
+                let mut key = String::new();
+                format_lua(&mut key, &k);
+
+                let mut value = String::new();
+                format_lua(&mut value, &v);
+
+                out.push_str(&format!("{}: {}", key, value));
+
+                first = false;
+            }
+            out.push_str("}");
+        },
+        AnyLuaValue::LuaOther => out.push_str("LuaOther"),
+    }
+}
+
 pub fn print(lua: &mut hlua::Lua, _: State) {
     // this function doesn't print to the terminal safely
     // only use this for debugging
     lua.set("print", hlua::function1(move |val: AnyLuaValue| {
-        println!("{:?}", val);
+        // println!("{:?}", val);
+        let mut out = String::new();
+        format_lua(&mut out, &val);
+        println!("{}", out);
     }))
 }
 
