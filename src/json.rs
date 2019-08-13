2eeb9962d3c91e8d982ca74ee3b31dc8c47c481f
kpcyrd <git@rxv.cc>
2019-08-13 16:05:56 +0200

    Bump dependencies

--- src/json.rs
+++ src/json.rs
@@ -104,9 +104,8 @@ impl From<AnyLuaValue> for LuaJsonValue {
     }
 }
 
-impl Into<serde_json::Value> for LuaJsonValue {
-    fn into(self) -> serde_json::Value {
-        use serde_json::Value;
+impl Into<Value> for LuaJsonValue {
+    fn into(self) -> Value {
         match self {
             LuaJsonValue::Null => Value::Null,
             LuaJsonValue::Bool(v) => Value::Bool(v),
@@ -123,9 +122,8 @@ impl Into<serde_json::Value> for LuaJsonValue {
     }
 }
 
-impl From<serde_json::Value> for LuaJsonValue {
-    fn from(x: serde_json::Value) -> LuaJsonValue {
-        use serde_json::Value;
+impl From<Value> for LuaJsonValue {
+    fn from(x: Value) -> LuaJsonValue {
         match x {
             Value::Null => LuaJsonValue::Null,
             Value::Bool(v) => LuaJsonValue::Bool(v),
--- src/ulimit.rs
+++ src/ulimit.rs
@@ -1,21 +1,21 @@
 // temporary solution until nix has ulimit support
 // https://github.com/nix-rust/nix/pull/879
 use std::mem;
-use libc::{self, c_int, rlimit, RLIM_INFINITY, rlim_t};
+use libc::{self, c_uint, rlimit, RLIM_INFINITY, rlim_t};
 use crate::errors::Result;
 use nix::errno::Errno;
 
 
 #[derive(Debug)]
 #[allow(non_camel_case_types)]
-#[repr(i32)]
+#[repr(u32)]
 pub enum Resource {
     RLIMIT_NOFILE = libc::RLIMIT_NOFILE,
 }
 
 pub fn getrlimit(resource: Resource) -> Result<(Option<rlim_t>, Option<rlim_t>)> {
     let mut rlim: rlimit = unsafe { mem::uninitialized() };
-    let res = unsafe { libc::getrlimit(resource as c_int, &mut rlim as *mut _) };
+    let res = unsafe { libc::getrlimit(resource as c_uint, &mut rlim as *mut _) };
     let rlim = Errno::result(res).map(|_| {
         (if rlim.rlim_cur != RLIM_INFINITY { Some(rlim.rlim_cur) } else { None },
          if rlim.rlim_max != RLIM_INFINITY { Some(rlim.rlim_max) } else { None })
@@ -28,7 +28,7 @@ pub fn setrlimit(resource: Resource, soft_limit: Option<rlim_t>, hard_limit: Opt
     rlim.rlim_cur = soft_limit.unwrap_or(RLIM_INFINITY);
     rlim.rlim_max = hard_limit.unwrap_or(RLIM_INFINITY);
 
-    let res = unsafe { libc::setrlimit(resource as c_int, &rlim as *const _) };
+    let res = unsafe { libc::setrlimit(resource as c_uint, &rlim as *const _) };
     Errno::result(res).map(drop)?;
     Ok(())
 }
