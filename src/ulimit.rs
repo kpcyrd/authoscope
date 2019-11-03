fcd097b28a2ea68407190e15e746cdbf360fff6b
Rui Chen <rchen@meetup.com>
2019-11-03 17:43:00 -0500

    Fix build issue

--- src/ulimit.rs
+++ src/ulimit.rs
@@ -4,18 +4,19 @@ use std::mem;
 use libc::{self, c_uint, rlimit, RLIM_INFINITY, rlim_t};
 use crate::errors::Result;
 use nix::errno::Errno;
+use std::convert::TryInto;
 
 
 #[derive(Debug)]
 #[allow(non_camel_case_types)]
-#[repr(u32)]
+#[repr(i32)]
 pub enum Resource {
     RLIMIT_NOFILE = libc::RLIMIT_NOFILE,
 }
 
 pub fn getrlimit(resource: Resource) -> Result<(Option<rlim_t>, Option<rlim_t>)> {
     let mut rlim: rlimit = unsafe { mem::uninitialized() };
-    let res = unsafe { libc::getrlimit(resource as c_uint, &mut rlim as *mut _) };
+    let res = unsafe { libc::getrlimit((resource as c_uint).try_into().unwrap(), &mut rlim as *mut _) };
     let rlim = Errno::result(res).map(|_| {
         (if rlim.rlim_cur != RLIM_INFINITY { Some(rlim.rlim_cur) } else { None },
          if rlim.rlim_max != RLIM_INFINITY { Some(rlim.rlim_max) } else { None })
@@ -28,7 +29,7 @@ pub fn setrlimit(resource: Resource, soft_limit: Option<rlim_t>, hard_limit: Opt
     rlim.rlim_cur = soft_limit.unwrap_or(RLIM_INFINITY);
     rlim.rlim_max = hard_limit.unwrap_or(RLIM_INFINITY);
 
-    let res = unsafe { libc::setrlimit(resource as c_uint, &rlim as *const _) };
+    let res = unsafe { libc::setrlimit((resource as c_uint).try_into().unwrap(), &rlim as *const _) };
     Errno::result(res).map(drop)?;
     Ok(())
 }
